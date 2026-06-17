3ba4450096afec516a80160091d8d6cb62cf7bd3
Ravindu Madusanka <143364364+Ravindumadu@users.noreply.github.com>
2026-06-17 23:34:15 +0530

    variable and print  second C language code

---
+++ second.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  char name[] = "Ravindu Madushanka"; //variable
+  char location[] = "matara, sri lanka";
+  int age = 23;
+
+  printf("My Name Is %s.\n", name);
+  printf("My location Is %s.\n", location);
+  printf("My Age Is %d Years Old.\n", age);
+
+  return 0;
+  
+}
