aec62f23e7d18141ffb1d3573d8badea93185286
Ravindu Madusanka <143364364+Ravindumadu@users.noreply.github.com>
2026-06-17 23:36:28 +0530

    Update second.c

--- second.c
+++ second.c
@@ -4,10 +4,12 @@ int main() {
   char name[] = "Ravindu Madushanka"; //variable
   char location[] = "matara, sri lanka";
   int age = 23;
+  float hieght = 5.3;
 
   printf("My Name Is %s.\n", name);
   printf("My location Is %s.\n", location);
   printf("My Age Is %d Years Old.\n", age);
+  printf("My Hieght Is %.1f.\n", hieght);
 
   return 0;
   
