1e819cdf44cbcd56cca9f3eb6f688509c71c01b0
Ravindu Madusanka <143364364+Ravindumadu@users.noreply.github.com>
2026-06-18 17:56:12 +0530

    Boolean Learn C

---
+++ third.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main () {
+  bool isOnline = false ; 
+
+  if(isOnline) {
+    printf("You Are Is ONLINE");
+  } else {
+    printf("You Are Is OFFLINE");
+  }
+}
