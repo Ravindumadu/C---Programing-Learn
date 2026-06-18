b191fecb338ab84c055227c96d672e1431af406d
Ravindu Madusanka <143364364+Ravindumadu@users.noreply.github.com>
2026-06-18 18:34:06 +0530

    Create four.c

---
+++ four.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+  int num1 = 1;
+  int num2 = 10;
+  int num3 = 100;
+
+  printf("%3d\n", num1); //width , pecision, falg
+  printf("%3d\n", num2);
+  printf("%3d\n", num3);
+}
